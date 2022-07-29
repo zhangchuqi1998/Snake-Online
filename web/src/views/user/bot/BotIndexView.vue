1adb10e673188672b3b50f7d90fa7b1b4c894980
aaron <yx163512@163.com>
2022-07-28 21:17:12 -0700

    bot backend

--- web/src/views/user/bot/BotIndexView.vue
+++ web/src/views/user/bot/BotIndexView.vue
@@ -7,10 +7,64 @@
 
 <script>
     import ContentField from "../../../components/ContentField.vue"
+    import $ from 'jquery'
+    import { useStore } from "vuex"
 
     export default {
         components: {
             ContentField
+        },
+        setup() {
+            const store = useStore();
+            // $.ajax({
+            //     url: "http://127.0.0.1:3000/user/bot/add/",
+            //     type: "POST",
+            //     data: {
+            //         title: "bot title",
+            //         description: "bot description",
+            //         content: "bot content",
+            //     },
+            //     headers: {
+            //         Authorization: "Bearer " + store.state.user.token,
+            //     },
+            //     success(resp) {
+            //         console.log(resp);
+            //     },
+            //     error(resp) {
+            //         console.log(resp);
+            //     }
+            // })
+
+            // $.ajax({
+            //     url: "http://127.0.0.1:3000/user/bot/remove/",
+            //     type: "POST",
+            //     data: {
+            //         bot_id: 3,
+            //     },
+            //     headers: {
+            //         Authorization: "Bearer " + store.state.user.token,
+            //     },
+            //     success(resp) {
+            //         console.log(resp);
+            //     },
+            //     error(resp) {
+            //         console.log(resp);
+            //     }
+            // })
+
+            $.ajax({
+                url: "http://127.0.0.1:3000/user/bot/getlist/",
+                type: "GET",
+                headers: {
+                    Authorization: "Bearer " + store.state.user.token,
+                },
+                success(resp) {
+                    console.log(resp);
+                },
+                error(resp) {
+                    console.log(resp);
+                }
+            })
         }
     }
 </script>
