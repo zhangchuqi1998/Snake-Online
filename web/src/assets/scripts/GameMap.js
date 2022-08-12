1107b1f35c5e073ac73fa00fadae98f15dba395e
aaron <yx163512@163.com>
2022-08-11 21:18:48 -0500

    game matching

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -3,11 +3,12 @@ import { Wall } from "./Wall";
 import { Snake } from "./Snake";
 
 export class GameMap extends AcGameObject {
-    constructor(ctx, parent) {
+    constructor(ctx, parent, store) {
         super();
 
         this.ctx = ctx;
         this.parent = parent;  
+        this.store = store;
         this.L = 0;
 
         this.rows = 13;
@@ -38,34 +39,8 @@ export class GameMap extends AcGameObject {
     }
 
     create_walls() {
-        const g = [];
-        for (let r = 0; r < this.rows; r++) {
-            g[r] = [];
-            for (let c = 0; c < this.cols; c++) {
-                g[r][c] = false;
-            }
-        }
-        for (let r = 0; r < this.rows; r++) {
-            g[r][0] = g[r][this.cols-1] = true;
-        }
 
-        for (let c = 0; c < this.cols; c++) {
-            g[0][c] = g[this.rows-1][c] = true;
-        }
-
-        for (let i = 0; i < this.inner_walls_count / 2; i++) {
-            for (let j = 0; j < 1000; j++) {
-                let r = parseInt(Math.random() * this.rows);
-                let c = parseInt(Math.random() * this.cols);
-                if (g[r][c] || g[this.rows-1-r][this.cols-1-c]) continue;
-                if (r == this.rows-2 && c == 1 || r == 1 && c == this.cols-2) continue;
-                g[r][c] = g[this.rows-1-r][this.cols-1-c] = true;
-                break;
-            }
-        }
-
-        const copy_g = JSON.parse(JSON.stringify(g));
-        if (!this.check_connectivity(copy_g, this.rows-2, 1, 1, this.cols-2)) return false;
+        const g = this.store.state.pk.gamemap;
 
         for (let r = 0; r < this.rows; r++) {
             for (let c = 0; c < this.cols; c++) {
@@ -74,7 +49,6 @@ export class GameMap extends AcGameObject {
                 }
             }
         }
-        return true;
     }
 
     add_listening_events() {
@@ -94,9 +68,7 @@ export class GameMap extends AcGameObject {
     }
 
     start() {
-        for (let i = 0; i < 1000; i++)           
-            if (this.create_walls())
-                break;
+        this.create_walls();
 
         this.add_listening_events();
     }
